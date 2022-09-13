bda45dd8158b1309c6b3741ebc31385851435613
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-13 16:37:24 -0500

    initial commit

---
+++ SelectionStatementsExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿var r = new Random();
+var favNumber = r.Next(1, 1000);
+//var favNumber = 33;
+
+Console.Write("Gues what my favorite number is: ");
+var userInput = int.Parse(Console.ReadLine());
+
+
+while (userInput != favNumber)
+{
+    if (userInput > favNumber)
+    {
+        Console.Write("Too high. Guess again: ");
+        userInput = int.Parse(Console.ReadLine());
+    }
+    else if (userInput < favNumber)
+    {
+        Console.Write("Too low. Guess again: ");
+        userInput = int.Parse(Console.ReadLine());
+    }
+}
+Console.WriteLine("You guessed it!");
